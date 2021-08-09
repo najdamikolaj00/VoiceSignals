9bede849af5e90ac70cdf664fb40c617abbfd63d
Mikołaj Najda <najda.mikolaj00@gmail.com>
2021-08-09 15:40:48 +0200

    Small changes in converter.py file and adding voice data

--- preprocessing/converter.py
+++ preprocessing/converter.py
@@ -7,7 +7,7 @@ from pydub import AudioSegment
 import os
 
 def converter():
-    rootdir = 'DataVoices'
+    rootdir = 'Data'
 
     for subdir, dirs, files in os.walk(rootdir):
         for filename in files:
@@ -16,4 +16,6 @@ def converter():
             else:
                 AudioSegment.from_file(os.path.join(subdir, filename), os.path.splitext(filename)[1][1:]).export(os.path.join(subdir
                 , filename.split(".")[0] + '.wav'), format = 'wav')
+                os.remove((os.path.join(subdir, filename)))
+                
 converter()
