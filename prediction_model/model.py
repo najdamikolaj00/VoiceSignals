b20ad67d260734d7ff8ac21a48c059adaa291e03
jakubzolkos <zolkos@bu.edu>
2021-09-24 20:49:47 -0400

    Neural network compilation method  added

--- prediction_model/model.py
+++ prediction_model/model.py
@@ -19,10 +19,10 @@ import splitfolders
 # import keras
 # import warnings
 # warnings.filterwarnings('ignore')
-# from keras import layers
-# from keras.layers import Activation, Dense, Dropout, Conv2D, Flatten, MaxPooling2D, GlobalMaxPooling2D, GlobalAveragePooling1D, AveragePooling2D, Input, Add
-# from keras.models import Sequential
-# from keras.optimizers import SGD
+from keras import layers
+from keras.layers import Activation, Dense, Dropout, Conv2D, Flatten, MaxPooling2D, GlobalMaxPooling2D, GlobalAveragePooling1D, AveragePooling2D, Input, Add
+from keras.models import Sequential
+from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 
 
@@ -80,8 +80,35 @@ class Sobriety():
                                                                   class_mode='categorical',
                                                                   shuffle = False )
 
-    def fit(self, x_train, y_train):
+    def compile_network(self):
         """ Fit train data to the model """
+
+        model = Sequential()
+        input_shape=(64, 64, 3)#1st hidden layer
+        model.add(Conv2D(32, (3, 3), strides=(2, 2), input_shape=input_shape))
+        model.add(AveragePooling2D((2, 2), strides=(2,2)))
+        model.add(Activation('relu'))#2nd hidden layer
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(AveragePooling2D((2, 2), strides=(2,2)))
+        model.add(Activation('relu'))#3rd hidden layer
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(AveragePooling2D((2, 2), strides=(2,2)))
+        model.add(Activation('relu'))#Flatten
+        model.add(Flatten())
+        model.add(Dropout(rate=0.5))#Add fully connected layer.
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(Dropout(rate=0.5))#Output layer
+        model.add(Dense(10))
+        model.add(Activation('softmax'))model.summary()
+
+        epochs = 200
+        batch_size = 8
+        learning_rate = 0.01
+        decay_rate = learning_rate / epochs
+        momentum = 0.9
+        sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
+        model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=['accuracy'])
         
 
     def predict(self, x_test, y_test):
