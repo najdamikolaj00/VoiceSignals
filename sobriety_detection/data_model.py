eb755f0bbbf77294b5ae3caec2b2274d04f51b64
jakubzolkos <zolkos@bu.edu>
2021-10-11 10:34:32 -0400

    Organizational Update

---
+++ sobriety_detection/data_model.py
@@ -0,0 +1,135 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import SelectKBest
+from sklearn.metrics import accuracy_score
+from catboost import CatBoostClassifier
+
+class Data_model(object):
+
+    def __init__(self, y, X, y_test_data, X_test_data):
+        
+        '''
+        Operator
+        '''
+
+        '''
+        Program required this two variables:
+        '''
+        self.y = y # labels = categorical values
+        self.X = X # features = numerical values
+
+        '''
+        If we want to check real predctions we use dataset from different time.
+        So we need to provide this two variables to model (but they are not necessary if
+        you are working only on training set):
+        '''
+        self.y_test_data = y_test_data
+        self.X_test_data = X_test_data
+
+        '''
+        Program will use this variables if you are working only on training set and
+        you are using train_test_split:
+        '''
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+
+        '''
+        Program will use this variables if you are going to do feature_selection with ANOVA method:
+        '''
+
+        self.X_train_fs = None
+        self.X_test_fs = None
+        self.y_test_fs = None
+
+    def train_split(self, test_size, random_state):
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size = test_size, random_state = random_state)
+
+    def feature_selection(self, method):
+
+        if method == 'ANOVA':
+            if self.X_test_data is not None:
+
+                fs = SelectKBest(score_func = f_classif, k = 4)
+                fs.fit(self.X, self.y)
+                self.X_train_fs = fs.transform(self.X)
+                self.X_test_fs = fs.transform(self.X_test_data)
+                self.y_test_fs = self.y_test_data
+
+            elif self.X_train is not None:
+
+                fs = SelectKBest(score_func = f_classif, k = 4)
+                fs.fit(self.X_train, self.y_train)
+                self.X_train_fs = fs.transform(self.X_train)
+                self.X_test_fs = fs.transform(self.X_test)
+                self.y_test_fs =self.y_test
+
+    def model_class_forest(self, n_estimators, max_depth, random_state):
+
+        if self.X_train_fs is not None and self.X_train is not None:
+            
+            model = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, random_state = random_state)
+            model.fit(self.X_train_fs, self.y_train)
+            prediction_values = model.predict(self.X_test_fs)
+            accuracy = accuracy_score(self.y_test_fs, prediction_values)
+            print('Accuracy: %.2f' % (accuracy*100))
+
+        elif self.X_test_data is not None and self.X_train_fs is not None:
+
+            model = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, random_state = random_state)
+            model.fit(self.X_train_fs, self.y)
+            prediction_values = model.predict(self.X_test_fs)
+            accuracy = accuracy_score(self.y_test_data, prediction_values)
+            print('Accuracy: %.2f' % (accuracy*100))
+
+        elif self.X_test_data is not None and self.X_train_fs is None:
+
+            model = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, random_state = random_state)
+            model.fit(self.X, self.y)
+            prediction_values = model.predict(self.X_test_data)
+            accuracy = accuracy_score(self.y_test_data, prediction_values)
+            print('Accuracy: %.2f' % (accuracy*100))
+
+        else:
+
+            model = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, random_state = random_state)
+            model.fit(self.X_train, self.y_train)
+            prediction_values = model.predict(self.X_test)
+            accuracy = accuracy_score(self.y_test, prediction_values)
+            print('Accuracy: %.2f' % (accuracy*100))
+
+    #tmp out of use
+    # def model_class_catboost(self):
+
+    #     if self.X_train_fs is not None:
+
+    #         model = CatBoostClassifier()
+    #         model.fit(self.X_train_fs, self.y_train)
+    #         prediction_values = model.predict(self.X_test_fs)
+    #         print(self.y_test_data)
+    #         print(prediction_values)
+    #         accuracy = accuracy_score(self.y_test_fs, prediction_values)
+    #         print('Accuracy: %.2f' % (accuracy*100))
+
+    #     elif self.X_test_data is not None:
+
+    #         model = CatBoostClassifier()
+    #         model.fit(self.X, self.y)
+    #         prediction_values = model.predict(self.X_test_data)
+    #         print(self.y_test_data)
+    #         print(prediction_values)
+    #         accuracy = accuracy_score(self.y_test_data, prediction_values)
+    #         print('Accuracy: %.2f' % (accuracy*100))
+
+    #     else:
+
+    #         model = CatBoostClassifier()
+    #         model.fit(self.X_train, self.y_train)
+    #         prediction_values = model.predict(self.X_test)
+    #         accuracy = accuracy_score(self.y_test, prediction_values)
+    #         print('Accuracy: %.2f' % (accuracy*100))
+            
---
+++ sobriety_detection/main.py
@@ -0,0 +1,84 @@
+'''
+Steps to make:
+1. Load files
+2. Check if they are already converted to .wav extension if they are not --> convert them
+3. Make a .csv file
+4. Feature extraction
+5. Save features to a csv file
+6. Data preprocessing
+7. Model approach
+8. Check the scores, save results
+9. Save the results and find different solutions of data preprocessing (so back to step 6.)
+
+Program will contain classes responsible for different steps 
+First class --> steps: 1, 2, 3, 5, 9.
+Second class --> steps: 4.
+Third class --> steps: 6.
+Fourth class --> steps: 7, 8.
+'''
+<<<<<<< HEAD
+
+from data_operator import Data_Operator
+
+
+def main():
+
+    obj = Data_operator('data_test2')
+    obj.import_audio()
+=======
+
+from data_operator import Data_operator
+from data_preprocessing import Data_processing
+from data_model import Data_model
+
+def main():
+    
+    '''
+    Test on men's voice: model learns on data collected one month ago before collecting test dataset.
+    '''
+
+    '''
+    Data from August(model is learning on them):
+    '''
+    obj = Data_operator('voice_data\data_test5', 'features_in_csv\datasetwojtek_august.csv')
+    obj.import_audio()
+
+    '''
+    Data from Semptember(we are going to check accuracy of predicion values)
+    '''
+
+    obj = Data_operator('voice_data\data_test6', 'features_in_csv\datasetwojtek_september.csv')
+    obj.import_audio()
+
+    '''
+    Our data variables: 
+    Input variables 'numerical': all features are numerical variables
+    Output variable 'target(categorical)': sobriety =  sober or unsober
+
+    It means that feature selection method should be ANOVA or Kendall's
+
+    ANOVA correlation coefficient (linear)
+    Kendall's rank coefficient (nonlinear)
+    '''
+
+    obj_2 = Data_processing('features_in_csv\datasetwojtek_august.csv')
+    y, X = obj_2.transform_data()
+
+    obj_test = Data_processing('features_in_csv\datasetwojtek_september.csv')
+    y_test, X_test = obj_test.transform_data()
+
+    #Sklearn approach 66.67 accuracy parameters: 12, 2, 5
+    obj_3 = Data_model(y, X, y_test, X_test)
+    # obj_3.train_split(0.25, 1)
+    # obj_3.feature_selection('ANOVA')
+    obj_3.model_class_forest(12, 2, 5)
+>>>>>>> 242cb8f1b9a34018a356940a10a174c092a454c3
+
+    #Catboost approach accuracy//tmp out of use
+    # obj_3 = Data_model(y, X, y_test, X_test)
+    # obj_3.train_split(0.25, 1)
+    # obj_3.feature_selection('ANOVA')
+    # obj_3.model_class_catboost()
+
+if __name__ == '__main__':
+    main()
