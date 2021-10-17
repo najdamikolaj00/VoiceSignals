5d0a81c9dc43cdab1b712ebfbd643930ba55422d
Mikołaj Najda <najda.mikolaj00@gmail.com>
2021-10-17 17:32:01 +0200

    new .csv files

--- sobriety_detection/main.py
+++ sobriety_detection/main.py
@@ -4,6 +4,7 @@ from data_preprocessing import Data_processing
 from data_model import Data_model
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 def main():
     
@@ -11,7 +12,7 @@ def main():
     Second value required dictionary name and .csv file name like: (id)(age)(sex)(number of  a test) from csv file idagesextest.csv
     '''
     # converter()
-    # obj = Data_operator('voice_data', '221M4.csv', 'windows')
+    # obj = Data_operator('voice_data', '221M65.csv', 'windows')
     # obj.import_audio()
 
     '''
@@ -30,10 +31,15 @@ def main():
     # data.to_csv('221Mlearn.csv', mode = 'a', header=False, index=False)
     # data_1.to_csv('221Mlearn.csv', mode = 'a', header=False, index=False)
     # data_2.to_csv('221Mlearn.csv', mode = 'a', header=False, index=False)
+    # data_1 = pd.read_csv('features_in_csv/BadanieTekst/221M65.csv')
+    # data_2 = pd.read_csv('features_in_csv/BadanieTekst/221M5.csv')
+    # data_1.to_csv('221Mlearn.csv', mode = 'a', header=False, index=False)
+    # data_2.to_csv('221Mlearn.csv', mode = 'a', header=False, index=False)
+    
     obj_train = Data_processing('221Mlearn.csv')
     y_train, X_train = obj_train.transform_data()
-    
-    obj_test = Data_processing('features_in_csv/BadanieTekst/221M4.csv')
+
+    obj_test = Data_processing('221MTest.csv')
     y_test, X_test = obj_test.transform_data()
     
     # model = Data_model(y_train, X_train, y_test, X_test)
@@ -41,6 +47,8 @@ def main():
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
     prediction_values = model.predict(X_test)
+    accuracy = accuracy_score(y_test, prediction_values)
+    print('Accuracy: %.2f' % (accuracy*100))
     
     print(prediction_values)
        
