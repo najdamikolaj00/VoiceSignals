a8ff650a69a1d9b1e83865a269bc42edb8d0de3b
jakubzolkos <zolkos@bu.edu>
2021-10-28 19:44:45 -0400

    Minor changes - added an example how to use preprocessing class

--- sobriety_detection/main.py
+++ sobriety_detection/main.py
@@ -1,4 +1,4 @@
-from data_model import SobrietyModel
+from model import SobrietyModel
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from catboost import CatBoostClassifier
@@ -16,19 +16,7 @@ def main():
     
     '''
     Second value required dictionary name and .csv file name like: (id)(age)(sex)(number of  a test) from csv file idagesextest.csv
-    '''
-    # converter()
-    # noisereduce()
-    # audio_split()
 
-    
-    # obj = Data_operator('folder_name', 'filename.csv', 'operating system')
-    # obj.import_audio()
-    # data = pd.read_csv('filename.csv')
-    # data.to_csv('filename.csv', mode = 'a', header=False, index=False)
-    
-
-    '''
     Our data variables: 
     Input variables 'numerical': all features are numerical variables
     Output variable 'target(categorical)': sobriety =  sober or unsober
@@ -39,8 +27,6 @@ def main():
     Kendall's rank coefficient (nonlinear)
     '''
 
-    
-
     # #Sklearn approach 66.67 accuracy parameters: 12, 2, 5
     # obj_3 = Data_model(y, X, y_test, X_test)
     # obj_3.train_split(0.25, 1)
@@ -49,15 +35,26 @@ def main():
 
 
     #Catboost approach accuracy//tmp out of use
-    # obj_3 = Data_model(y, X, y_test, X_test)
+    # obj_3 = Data_model(y, X, y_test, X_test
     # obj_3.train_split(0.25, 1)
     # obj_3.feature_selection('ANOVA')
     # obj_3.model_class_catboost()
 
 
-    data = AudioPreprocessing()
-    X_train, X_test, y_train, y_test = data.model_data_split("../features_in_csv/025frames/mixed025all.csv")
+    ''' Below is an example how to use the AudioPreprocessing class'''
+   
+    '''
+    data = AudioPreprocessing() # Initializes the class instance
 
+    data.import_audio(SOME_PATH) # Imports all the audio files contained in the folder specified by the path (converts non .wav to .wav too)
+
+    data.save_csv(SOME_PATH_WITH_FILENAME) # Extracts all the features from the loaded audio files and stores them in a .csv where each loaded file has its own row with data
+                                     # If a file from a given row didn't have "sober" or "unsober" in the filename, the label for that row will be "unknown"
+
+    # Performs a train-test split on the data from the specified .csv file - data ready to be fed to the model
+    X_train, X_test, y_train, y_test = data.model_data_split("../features_in_csv/025frames/mixed025all.csv")
+    '''
+    
     model = xgb.XGBClassifier()
     # model = RandomForestClassifier()
     # model = CatBoostClassifier()
