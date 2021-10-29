3784f1ece68c45765c6a4f234438feafcbf4b492
jakubzolkos <zolkos@bu.edu>
2021-10-28 20:33:58 -0400

    Files updated

--- sobriety_detection/main.py
+++ sobriety_detection/main.py
@@ -3,14 +3,14 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from catboost import CatBoostClassifier
 from sklearn.metrics import accuracy_score
-from audio_split import audio_split
-from noisereduction import noisereduce
 import os
 import sys
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 sys.path.insert(0, '../preprocessing')
 from audio_preprocessing import AudioPreprocessing
+from audio_split import audio_split
+from noisereduction import noisereduce
 
 
 '''
@@ -31,13 +31,6 @@ Second class --> steps: 4.
 Third class --> steps: 6.
 Fourth class --> steps: 7, 8.
 '''
-
-from converter import converter
-from data_operator import Data_operator
-from data_preprocessing import Data_processing
-from data_model import Data_model
-import pandas as pd
-
 
 def main():
     
@@ -71,17 +64,17 @@ def main():
 
     ''' Below is an example how to use the AudioPreprocessing class'''
    
-    '''
+    
     data = AudioPreprocessing() # Initializes the class instance
 
-    data.import_audio(SOME_PATH) # Imports all the audio files contained in the folder specified by the path (converts non .wav to .wav too)
+    # data.import_audio(SOME_PATH) # Imports all the audio files contained in the folder specified by the path (converts non .wav to .wav too)
 
-    data.save_csv(SOME_PATH_WITH_FILENAME) # Extracts all the features from the loaded audio files and stores them in a .csv where each loaded file has its own row with data
+    # data.save_csv(SOME_PATH_WITH_FILENAME) # Extracts all the features from the loaded audio files and stores them in a .csv where each loaded file has its own row with data
                                      # If a file from a given row didn't have "sober" or "unsober" in the filename, the label for that row will be "unknown"
 
     # Performs a train-test split on the data from the specified .csv file - data ready to be fed to the model
     X_train, X_test, y_train, y_test = data.model_data_split("../features_in_csv/025frames/mixed025all.csv")
-    '''
+    
     
     model = xgb.XGBClassifier()
     # model = RandomForestClassifier()
